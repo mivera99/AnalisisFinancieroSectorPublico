ad16bb74ac0d44d5d7f5e24d17b01c035668615b
GitWAMH <61278893+GitWAMH@users.noreply.github.com>
2021-09-21 22:33:25 +0200

    Aumento de la BBDD
    Falta guardar correctamente la fecha

--- index.php
+++ index.php
@@ -14,16 +14,50 @@ foreach ($xml->children() as $row) {
     $NOMBRE_CCAA = $row->NOMBRE_CCAA;
     $POBLACION_2017 = $row->POBLACION_2017;
     $NOMBREPRESIDENTE = $row->NOMBREPRESIDENTE;
+    $APELLIDO1PRESIDENTE = $row->APELLIDO1PRESIDENTE;
+    $APELLIDO2PRESIDENTE = $row->APELLIDO2PRESIDENTE;
     
+    $tiempo = strtotime($row->VIGENCIA);
+    $VIGENCIA=date('Y-m-d',$tiempo);
+    
+    $PARTIDO = $row->PARTIDO;
+    $CIF = $row->CIF;
+    $TIPOVIA = $row->TIPOVIA;
+    $NOMBREVIA = $row->NOMBREVIA;
+    $NUMVIA = $row->NUMVIA;
+    $CODPOSTAL = $row->CODPOSTAL;
+    $TELEFONO = $row->TELEFONO;
+    $FAX = $row->FAX;
+    $WEB = $row->WEB;
+    $MAIL = $row->MAIL;
+    $REFPIB = $row->REFPIB;
+    $PIB = $row->PIB;
+    $REFPIBC = $row->REFPIBC;
+    $PIBC = $row->PIBC;
+    $REFRESULTADO = $row->REFRESULTADO;
+    $RESULTADO = $row->RESULTADO;
+    $REFDEUDAVIVA = $row->REFDEUDAVIVA;
+    $DEUDAVIVA = $row->DEUDAVIVA;
+
     echo($CODIGO_CCAA."<br>");
     echo($NOMBRE_CCAA."<br>");
     echo($POBLACION_2017."<br>");
     echo($NOMBREPRESIDENTE."<br>");
+    echo($APELLIDO1PRESIDENTE."<br>");
+    echo($APELLIDO2PRESIDENTE."<br>");
+    echo($VIGENCIA."<br>");
     echo("<br><br><br>");
     
 
 
-    $sql = "INSERT INTO bloque_general_ccaa(CODIGO_CCAA,NOMBRE_CCAA,POBLACION_2017,NOMBREPRESIDENTE) VALUES ('" . $CODIGO_CCAA . "','" . $NOMBRE_CCAA . "','" . $POBLACION_2017 . "','" . $NOMBREPRESIDENTE . "')";
+    $sql = "INSERT INTO bloque_general_ccaa(CODIGO_CCAA,NOMBRE_CCAA,POBLACION_2017,NOMBREPRESIDENTE,
+    APELLIDO1PRESIDENTE, APELLIDO2PRESIDENTE, VIGENCIA, PARTIDO, CIF, TIPOVIA, NOMBREVIA, NUMVIA, 
+    CODPOSTAL, TELEFONO,FAX, WEB, MAIL, REFPIB, PIB, REFPIBC, PIBC, REFRESULTADO, RESULTADO, REFDEUDAVIVA, 
+    DEUDAVIVA) VALUES ('" . $CODIGO_CCAA . "','" . $NOMBRE_CCAA . "','" . $POBLACION_2017 . "',
+    '" . $NOMBREPRESIDENTE . "','".$APELLIDO1PRESIDENTE."','".$APELLIDO2PRESIDENTE."','".$VIGENCIA."',
+    '".$PARTIDO."','".$CIF."','".$TIPOVIA."','".$NOMBREVIA."','".$NUMVIA."','".$CODPOSTAL."','".$TELEFONO."','".
+    $FAX."','".$WEB."','".$MAIL."','".$REFPIB."','".$PIB."','".$REFPIBC."','".$PIBC."','".$REFRESULTADO."',
+    '".$RESULTADO."','".$REFDEUDAVIVA."','".$DEUDAVIVA."')";
     
     $result = mysqli_query($conn, $sql);
     
