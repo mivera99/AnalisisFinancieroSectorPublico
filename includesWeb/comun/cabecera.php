f7f846b45f136f606703d256c9d7d07d702aa808
GitWAMH <61278893+GitWAMH@users.noreply.github.com>
2022-02-10 01:21:11 +0100

    Correciones implementadas

--- includesWeb/comun/cabecera.php
+++ includesWeb/comun/cabecera.php
@@ -1,12 +1,13 @@
 <div id="menu">
     <div id="menu-left">
         <a href="index.php">
-            <p class="title">Análisis Financiero del Sector Público</p>
+            <p class="title">Análisis financiero del sector público</p>
             <p class="subtitle">Noster Economia</p>
         </a>
     </div>
     <div id="menu-right">
         <!--<a href="#home">INFORMES</a>-->
+        <a href="index.php">INICIO</a>
         <a href="consultas.php">CONSULTAS</a>
         <a href="sobre_nosotros.php">SOBRE NOSOTROS</a>
         <a href="contacto.php">CONTACTO</a>
