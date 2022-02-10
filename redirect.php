f8955cac0a68168dff1cf7dd04dadb496cbc8276
GitWAMH <61278893+GitWAMH@users.noreply.github.com>
2022-02-10 19:49:11 +0100

    Update redirect.php

--- redirect.php
+++ redirect.php
@@ -1,7 +1,33 @@
 <?php
 $nombre = htmlspecialchars(trim(strip_tags($_REQUEST["facilities"])));
 
-$arr = explode(' (', $nombre);
+$facility = '';
+$tipo = '';
+
+if(strpos($nombre, '(CCAA)')){
+    $arr = explode('(CCAA)',$nombre);
+    $facility = trim($arr[0]); // Se eliminan los espacios en balcno al inicio y al final de la cadena
+    header('Location:infoCCAA.php?ccaa='.$facility.'');
+}
+else {
+    if(strpos($nombre, '(MUNICIPIO)')){
+        $arr = explode('(MUNICIPIO)',$nombre);
+        $facility = trim($arr[0]); // Se eliminan los espacios en balcno al inicio y al final de la cadena
+        header('Location:infoMunicipio.php?mun='.$facility.'');
+    }
+    else {
+        if(strpos($nombre, '(DIPUTACIÓN)')){
+            $arr = explode('(DIPUTACIÓN)',$nombre);
+            $facility = trim($arr[0]); // Se eliminan los espacios en balcno al inicio y al final de la cadena
+            header('Location:infoDiputacion.php?dip='.$facility.'');
+        }
+        else {
+            header('Location:index.php');
+        }
+    }
+}
+
+/*$arr = explode(' (', $nombre);
 $arr[1] = substr($arr[1], 0, -1);
 
 if(count($arr)==2){
@@ -21,5 +47,5 @@ if(count($arr)==2){
 else {
     header('Location:index.php');
 }
-
+*/
 ?>
