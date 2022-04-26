fd6cbb3660bc902134cfca9e3757355f9dcd639f
MiquelVera <mivera@ucm.es>
2022-04-26 09:05:33 +0200

    InicioRaro

--- index.php
+++ index.php
@@ -37,7 +37,7 @@ if (isset($_SESSION['mensaje'])) {
             <?php require("includesWeb/comun/cabecera.php");?>  
         </div>
 
-        <div id ="contenido">     
+        <!-- <div id ="contenido">     
             <h2>Informes financieros para la <b>transparencia</b> del sector público</h2>
             <p>Obten información detallada y actualizada sobre las comunidades autónomas, diputaciones y municipios.</p>
             <script>cargarNombres();</script>
@@ -50,11 +50,11 @@ if (isset($_SESSION['mensaje'])) {
             </form>
             <script>autocomplete(document.getElementById("facility"));</script>
             <?php
-                if (isset($show_message)) {
-                    echo "<script>alert('{$show_message}');</script>";
-                    $show_message=null;
-                }
-            ?>
+                // if (isset($show_message)) {
+                //     echo "<script>alert('{$show_message}');</script>";
+                //     $show_message=null;
+                // }
+            ?> -->
         <div id ="contenidoIni">     
         <?php require("includesWeb/comun/buscador.php");?>  
         </div>
