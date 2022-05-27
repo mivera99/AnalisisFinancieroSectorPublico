f5e766ab0be4c4bcf2e64fae712a4b95d8431536
GitWAMH <61278893+GitWAMH@users.noreply.github.com>
2022-05-27 21:09:13 +0200

    Escape de caracteres en la exportación

--- procesarExportacion.php
+++ procesarExportacion.php
@@ -5,7 +5,7 @@ $nombre = urldecode(htmlspecialchars(trim(strip_tags($_REQUEST['nombre']))));
 $type = htmlspecialchars(trim(strip_tags($_REQUEST['tipo'])));
 $cargador = new DAOCargador();
 
-$filename = $cargador->export($nombre,$type);
+$filename = $cargador->export(addslashes($nombre),$type);
 $nombre = str_replace(' ', '_', str_replace(',','',str_replace('-','',strtolower($nombre)))).'_ingresos.xlsx';
 header('Content-Type: text/xlsx; charset=utf-8');
 header('Content-Disposition: attachment; filename="'.$nombre.'";');
